c0f41bc43a9b82fada641d1e94db032761c9eb13
PaoloCardinale09 <alfiozeronove@gmail.com>
2023-03-03 17:21:39 +0100

    add fetch

---
+++ src/components/BaseSearch.vue
@@ -0,0 +1,49 @@
+<script>
+// import MyComponent from "./components/MyComponent.vue";
+
+export default {
+  data() {
+    return {
+      term: "",
+    };
+  },
+
+  emits: ["on-search"],
+
+  methods: {
+    clear() {
+      this.term = "";
+    },
+
+    search() {
+      this.$emit("on-search", this.term);
+    },
+  },
+
+  // components: {
+  //   MyComponent,
+  // },
+};
+</script>
+
+<template>
+  <form @submit.prevent="" class="py-3">
+    <div class="input-group">
+      <input
+        type="text"
+        class="form-control"
+        placeholder="search films"
+        v-model="term"
+        @keyup.enter="search"
+      />
+      <button class="btn btn-primary" type="button" @click="search">
+        Search
+      </button>
+      <button class="btn btn-secondary" type="button" @click="clear">
+        Reset
+      </button>
+    </div>
+  </form>
+</template>
+
+<style lang="scss" scoped></style>
--- src/components/FlimList.vue
+++ src/components/FlimList.vue
@@ -1,33 +1,59 @@
 <script>
 // import MyComponent from "./components/MyComponent.vue";
 import FilmCard from "./FilmCard.vue";
+import BaseSearch from "./BaseSearch.vue";
 import axios from "axios";
 
 export default {
   data() {
     return {
       films: [],
+      endpoint:
+        "https://api.themoviedb.org/3/search/movie?api_key=fc223a2987b967c2dd7d6b951473a5f7&query=",
     };
   },
 
   components: {
     FilmCard,
+    BaseSearch,
   },
+  // importato
+  methods: {
+    fetch(url) {
+      axios.get(url).then((response) => {
+        this.films = response.data.results;
+      });
+    },
+
+    fetchFilteredCards(term) {
+      // console.log(term);
+      this.fetch(`${this.endpoint} ${term}`);
+    },
+  },
+  // fine importato
 
   created() {
-    axios
-      .get(
-        "https://api.themoviedb.org/3/search/movie?api_key=fc223a2987b967c2dd7d6b951473a5f7&query=ritorno%20"
-      )
-      .then((response) => {
-        console.log(response.data.results[0].title);
-      });
+    this.fetch(this.endpoint);
   },
+
+  //   created() {
+  //     axios.get(this.endpoint).then((response) => {
+  //       this.film = response.data.results[0];
+  //       console.log("film+" + this.film.title);
+  //       console.log("endpoint" + this.endpoint);
+  //     });
+  //   },
 };
 </script>
 
 <template>
-  <FilmCard :title="response.data.results[0].title" />
+  <BaseSearch @on-search="fetchFilteredCards" />
+  <FilmCard
+    :title="this.films.title"
+    :originalTitle="this.films.original_title"
+    :language="this.films.original_language"
+    :rating="this.films.vote_average"
+  />
 </template>
 
 <style lang="scss" scoped></style>
